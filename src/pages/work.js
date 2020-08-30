274e8475887dab23d2d847877b8fd0599edb9d7e
Turkyden <616287091@qq.com>
2020-08-30 14:25:43 +0800

    Added the update

--- src/pages/work.js
+++ src/pages/work.js
@@ -10,32 +10,13 @@ const Work = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="Work" />
-      <h2 className="text-3xl text-center">开源项目</h2>
-      <div className="py-10">
-        <div class="max-w-sm w-full lg:max-w-full lg:flex">
-          <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{ backgroundImage: "url('https://tailwindcss.com/img/card-left.jpg')" }} title="Woman holding a mug">
-          </div>
-          <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-            <div class="mb-8">
-              <p class="text-sm text-gray-600 flex items-center">
-                <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                  <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                </svg>
-                Members only
-              </p>
-              <div class="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-              <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-            </div>
-            <div class="flex items-center">
-              <img class="w-10 h-10 rounded-full mr-4" src="https://tailwindcss.com/img/jonathan.jpg" alt="Avatar of Jonathan Reinink" />
-              <div class="text-sm">
-                <p class="text-gray-900 leading-none">Jonathan Reinink</p>
-                <p class="text-gray-600">Aug 18</p>
-              </div>
-            </div>
-          </div>
-        </div>
+      <img alt="statas card" src="https://github-readme-stats.vercel.app/api/?username=anuraghazra&show_icons=true&title_color=ed8936&icon_color=ed8936&text_color=9f9f9f&bg_color=151515" />
+      <div className="flex flex-wrap justify-between py-10">
+        <img className="" alt="repo card" src="https://github-readme-stats.vercel.app/api/pin?username=Turkyden&repo=vue-typical&title_color=fff&icon_color=f9f9f9&text_color=9f9f9f&bg_color=151515" />
+        <img className="" alt="repo card" src="https://github-readme-stats.vercel.app/api/pin?username=Turkyden&repo=imagehover-effects&title_color=fff&icon_color=f9f9f9&text_color=9f9f9f&bg_color=151515" />
+        <img className="" alt="repo card" src="https://github-readme-stats.vercel.app/api/pin?username=anuraghazra&repo=github-readme-stats&title_color=fff&icon_color=f9f9f9&text_color=9f9f9f&bg_color=151515" />
       </div>
+      
       <h2 className="text-3xl text-center">UI 设计</h2>
       <div className="py-10">
         <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
