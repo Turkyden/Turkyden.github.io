3ba9645d01515d30ebd0a4a554697f9724452b37
Turkyden <616287091@qq.com>
2020-09-11 01:17:59 +0800

    Added the dar mode

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,7 +18,7 @@ const Layout = ({ location, title, children }) => {
   return (
     <div className="bg-primary text-on-primary">
       <header className="container m-auto py-10 flex justify-between">
-        <h1 className="bg-white text-gray-900 text-2xl font-semibold px-2">
+        <h1 className="bg-on-primary text-primary text-2xl font-semibold px-2">
           <Link to={`/`}>
             DDJ
           </Link>
@@ -26,6 +26,7 @@ const Layout = ({ location, title, children }) => {
         
         <ul className="flex items-center">
           <li className="pl-8"><Link to={`/vlog`}>Vlog</Link></li>
+          <li className="pl-8"><Link to={`https://github.com/Turkyden/p6p7`} target="_blank">p6p7</Link></li>
           <li className="pl-8"><Link to={`/blog`}>专栏</Link></li>
           <li className="pl-8"><Link to={`/work`}>作品</Link></li>
           <li className="pl-8"><Link to={`/about`}>关于</Link></li>
